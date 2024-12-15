c847f0fb3e0abd2cd9e572f2bc75132fba9fdb04
anilson <anilsonmmonteiro@gmail.com>
2024-12-15 19:15:47 +0000

    Remove RMLMapper setup steps from setup script to streamline environment initialization

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,15 +4,6 @@
 git clone https://github.com/HULTIG/safir-self-adaptive-semantic-framework.git
 cd safir-self-adaptive-semantic-framework
 
-# Download the RMLMapper JAR
-wget https://github.com/RMLio/rmlmapper-java/releases/download/v6.5.1/rmlmapper-6.5.1-r371-all.jar
-
-# Create the necessary directory
-mkdir -p src/RelationalToRDFAPI/src/main/resources/jar/
-
-# Move the JAR to the appropriate directory
-mv rmlmapper-6.5.1-r371-all.jar src/RelationalToRDFAPI/src/main/resources/jar/
-
 # Start the environment - it will take some time to build the services for the first time due to dependencies
 docker-compose -f src/docker-compose.yml up -d --build
 
