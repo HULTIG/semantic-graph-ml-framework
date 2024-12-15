ca58d4cc7f4fa802ab18a794b616fa50ba653b20
anilson <anilsonmmonteiro@gmail.com>
2024-12-15 22:26:36 +0000

    Simplify setup script by removing unnecessary directory changes and comments for improved clarity

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,11 +2,9 @@
 
 # Clone the repository
 # git clone https://github.com/HULTIG/safir-self-adaptive-semantic-framework.git
-cd safir-self-adaptive-semantic-framework
+# cd safir-self-adaptive-semantic-framework
 
-cd src
-
-sudo chmod -R 777 ./fuseki-config/data
+# cd src
 
 # Start the environment - it will take some time to build the services for the first time due to dependencies
 docker-compose -f src/docker-compose.yaml up -d --build
